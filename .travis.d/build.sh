fa86760500ecdec5b46ceab50243f9538dc0a5c2
Joshua Jacob <joshualaurencio@gmail.com>
2019-05-28 20:20:13 -0700

    Add auto-build for dev

--- .travis.d/build.sh
+++ .travis.d/build.sh
@@ -1,1 +1,18 @@
 echo "Travis Init"
+
+echo "Setting up serverless..."
+# Installing the serverless cli
+npm install -g serverless
+# Updating serverless from a previous version of serverless
+npm update -g serverless
+npm install
+
+echo "Setting up AWS CLI..."
+# Instal AWS CLI
+pip3 install awscli --upgrade --user
+
+echo "Deploying Serverless Application..."
+pyenv virtualenv -p python3.6 3.6.3
+pyenv local otter-pond-ci
+
+serverless deploy --stage dev
