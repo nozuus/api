cab0c4f4c5f7edf616e3f86dd4d54ee76f320522
Daniel Becker <dbecker.fl@gmail.com>
2019-06-22 13:50:00 -0700

    Email cleaned up, admin emailing built in for errors and embedded emails, spam filter activated

--- core/services/emailer_service.py
+++ core/services/emailer_service.py
@@ -3,78 +3,88 @@ import core.database.users_db as users_db
 import boto3
 import email
 import json
+import os
+import datetime
 
 
 def process_received_email(mail):
-    message_id = mail["messageId"]
-    to_emails = mail["destination"]
-    print("To emails: ", to_emails)
-    metadata_from = mail["source"]
-    print("Metadata from: ", metadata_from)
-    destinations = []
-    total_emails = []
-    allow_external = True
-    for to_email in to_emails:
-        email_list = email_list_db.get_email_list_by_address(to_email)
-        if email_list is not None:
-            if not email_list["allow_external"]:
-                allow_external = False
-            user_emails = get_emails_for_list(to_email)
-            users_to_send_to = []
-            for user_email in user_emails:
-                # Don't add the user if they're already being sent it, or if
-                # they were the sender or are already a recipient
-                if (user_email not in total_emails
-                        and user_email != metadata_from
-                        and user_email not in to_emails):
-                    total_emails.append(user_email)
-                    users_to_send_to.append(user_email)
-            if len(users_to_send_to) > 0:
-                destinations.append((email_list["subject_prefix"],
-                                     users_to_send_to))
+    try:
+        print("Printing mail object: ")
+        print(mail)
+        message_id = mail["messageId"]
+        to_emails = mail["destination"]
+        print("To emails: ", to_emails)
+        metadata_from = mail["source"]
+        #Putting a hold on this for now
+        find_embedded_to_address(mail["headers"], to_emails)
+        print("Metadata from: ", metadata_from)
+        destinations = []
+        total_emails = []
+        allow_external = True
+        for to_email in to_emails:
+            email_list = email_list_db.get_email_list_by_address(to_email)
+            if email_list is not None:
+                if not email_list["allow_external"]:
+                    allow_external = False
+                user_emails = get_emails_for_list(to_email)
+                users_to_send_to = []
+                for user_email in user_emails:
+                    # Don't add the user if they're already being sent it, or if
+                    # they were the sender or are already a recipient
+                    if (user_email not in total_emails
+                            and user_email != metadata_from
+                            and user_email not in to_emails):
+                        total_emails.append(user_email)
+                        users_to_send_to.append(user_email)
+                if len(users_to_send_to) > 0:
+                    destinations.append((email_list["subject_prefix"],
+                                         users_to_send_to))
 
-    print("Destinations: ", destinations)
+        print("Destinations: ", destinations)
 
-    email_contents = read_email_from_s3(message_id)
-    msg = email.message_from_bytes(email_contents)
+        email_contents = read_email_from_s3(message_id)
+        msg = email.message_from_bytes(email_contents)
 
-    if len(destinations) == 0:
-        print("Invalid destinations. Sending bounce")
-        send_invalid_destination_email(metadata_from, to_emails, msg["Subject"])
-        return
-
-    if not allow_external:
-        if not check_valid_from_email(metadata_from):
-            print("Invalid from email. Sending bounce")
-            send_invalid_from_email(metadata_from, to_emails, msg["Subject"])
+        if len(destinations) == 0:
+            print("Invalid destinations. Sending bounce")
+            #send_invalid_destination_email(metadata_from, to_emails, msg["Subject"])
             return
 
-    msg_from = msg["From"]
-    from_name, from_email = split_from(msg_from)
-    del msg['Return-Path']
-    del msg['From']
-    del msg['Reply-To']
-    safe_from = '%s <mailer@email.theotterpond.com>' % (from_name)
-    user_from = "%s <%s>" % (from_name, from_email)
-    msg["Reply-To"] = user_from
-    msg["From"] = safe_from
-    msg["Return-Path"] = "mailer@email.theotterpond.com"
-    msg["Source"] = "mailer@email.theotterpond.com"
+        if not allow_external:
+            if not check_valid_from_email(metadata_from):
+                print("Invalid from email. Sending bounce")
+                #send_invalid_from_email(metadata_from, to_emails, msg["Subject"])
+                return
 
-    original_subject = msg["Subject"]
-    for subject_prefix, user_emails in destinations:
-        print("Original Subject: " + original_subject)
-        if subject_prefix is not None and subject_prefix not in original_subject:
-            new_subject = "[" + subject_prefix + "] " + original_subject
-            del msg["Subject"]
-            msg["Subject"] = new_subject
-            print("Adding subject: %s" % new_subject)
-        if len(user_emails) >= 50:
-            chunked_emails = chunks(user_emails, 50)
-            for user_chunks in chunked_emails:
-                send_email(msg, user_chunks)
-        else:
-            send_email(msg, user_emails)
+        msg_from = msg["From"]
+        from_name, from_email = split_from(msg_from)
+        del msg['Return-Path']
+        del msg['From']
+        del msg['Reply-To']
+        del msg['Source']
+        safe_from = '%s <mailer@email.theotterpond.com>' % (from_name)
+        user_from = "%s <%s>" % (from_name, from_email)
+        msg["Reply-To"] = user_from
+        msg["From"] = safe_from
+        msg["Return-Path"] = "mailer@email.theotterpond.com"
+        msg["Source"] = "mailer@email.theotterpond.com"
+
+        original_subject = msg["Subject"]
+        for subject_prefix, user_emails in destinations:
+            print("Original Subject: " + original_subject)
+            if subject_prefix is not None and subject_prefix not in original_subject:
+                new_subject = "[" + subject_prefix + "] " + original_subject
+                del msg["Subject"]
+                msg["Subject"] = new_subject
+                print("Adding subject: %s" % new_subject)
+            if len(user_emails) >= 50:
+                chunked_emails = chunks(user_emails, 50)
+                for user_chunks in chunked_emails:
+                    send_email(msg, user_chunks)
+            else:
+                send_email(msg, user_emails)
+    except:
+        send_admin_email()
 
 
 def split_from(msg_from):
@@ -174,6 +184,43 @@ def check_valid_from_email(from_email):
             return True
     return False
 
+
+def find_embedded_to_address(headers, to_emails):
+    print(headers)
+    for header in headers:
+        if header["name"] == "Received":
+            if "for " in header["value"]:
+                print("Found for address in received header. Parsing value")
+                value = header["value"]
+                address = header["value"][(value.index("for ") + 4):value.index(";")]
+                print("Address: " + address)
+                if address not in to_emails:
+                    send_admin_email()
+                return
+
+
+def send_admin_email():
+    admin_email = os.environ['admin_email']
+    if admin_email is None or admin_email == "":
+        return
+    message = {
+        'Subject': {
+            "Data": "Admin Email",
+        },
+        "Body": {
+            "Text": {
+                "Data": "Please check the logs from %s for an item needing "
+                        "attention." % (str(datetime.datetime.now()))
+
+            }
+        }
+    }
+
+    print(message)
+    email_client = boto3.client('ses')
+    email_client.send_email(Source= "Otter Pond Emailer <noreply@email.theotterpond.com>",
+                            Destination={"ToAddresses": [admin_email]},
+                            Message=message)
 
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
