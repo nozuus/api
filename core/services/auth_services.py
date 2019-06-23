4b5e776d7fb6a53d124d44d206a6570987feca4f
Daniel Becker <dbecker.fl@gmail.com>
2019-06-23 11:54:19 -0700

    Implemented initial authentication flow

--- core/services/auth_services.py
+++ core/services/auth_services.py
@@ -1,23 +1,28 @@
 import core.database.users_db as users_db
 from passlib.hash import pbkdf2_sha256 as sha256
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 
-# def check_login(user_email, password):
-#     user = users_db.get_user_by_email(user_email)
-#     if user is None or user["hashed_password"] is None:
-#         return False
-#     else:
-#         if sha256.verify(password, user["hashed_password"]):
-#             token = create_access_token(identity=user["user_id"])
-#             return token
-#         return False
-#
-#
-# def set_password(user_id, password):
-#     user = users_db.get_user_by_id(user_id)
-#     user["hashed_password"] = sha256.hash(password)
-#     if users_db.update_user(user):
-#         return
-#     else:
-#         raise Exception("Failed to create user")
+def check_login(user_email, password):
+    user = users_db.get_user_by_email(user_email)
+    if user is None or user["hashed_password"] is None:
+        return False
+    else:
+        if sha256.verify(password, user["hashed_password"]):
+            token = create_access_token(identity=user["pk"])
+            return token
+        return False
+
+
+def set_password(user_email, password):
+    user = users_db.get_user_by_email(user_email)
+    user["hashed_password"] = sha256.hash(password)
+    if users_db.update_user(user):
+        return
+    else:
+        raise Exception("Failed to create user")
+
+
+@jwt_required
+def get_identity():
+    return get_jwt_identity()
