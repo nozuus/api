875379399ff63b29755d4b56437cc1b3b64b3ad9
Joshua Jacob <joshualaurencio@gmail.com>
2019-06-25 20:40:43 -0700

    Modify build command to include email

--- .travis.d/build.sh
+++ .travis.d/build.sh
@@ -8,11 +8,16 @@ npm update -g serverless
 npm install
 
 echo "Setting up AWS CLI..."
-# Instal AWS CLI
 pip3 install awscli --upgrade --user
 
 echo "Deploying Serverless Application..."
-pyenv virtualenv -p python3.6 3.6.3
-pyenv local otter-pond-ci
 
-serverless deploy --stage dev
+if [ "$TRAVIS_BRANCH" = "$DEV_BRANCH_NAME" ]; then
+  TARGET_BRANCH="$DEV_BRANCH_TARGET"
+elif [ "$TRAVIS_BRANCH" = "$PROD_BRANCH_NAME" ]; then
+  TARGET_BRANCH="$PROD_BRANCH_TARGET"
+else
+  exit 1
+fi
+
+serverless deploy --stage $TARGET_BRANCH --admin_email=$ADMIN_EMAIL
